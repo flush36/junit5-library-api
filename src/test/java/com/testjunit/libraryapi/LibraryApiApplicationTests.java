085bc9f1a9160fd6de8fb83f5c40e8725fd1cb80
Flush36 <thiagoferreira.dev@gmail.com>
2022-04-28 08:02:32 -0300

    first commit

---
+++ src/test/java/com/testjunit/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testjunit.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
